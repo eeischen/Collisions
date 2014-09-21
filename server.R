1714a5a83916b88fb72ec84900ee8a89ef7ac0f1
Ellen Eischen <eeischen@alumni.princeton.edu>
2014-09-20 20:07:01 -0400

    added app files

---
+++ server.R
@@ -0,0 +1,82 @@
+#The server code for my NYC collisions app.
+#Include packages
+require(devtools)
+library(rCharts)
+library(manipulate)
+library(shiny)
+suppressPackageStartupMessages(library(googleVis))
+require(googleVis)
+library(reshape)
+library(UsingR)
+
+#The collisions data was downloaded from 
+#https://data.cityofnewyork.us/NYC-BigApps/NYPD-Motor-Vehicle-Collisions/h9gi-nx95
+#That website is updated regularly with new data, but this app currently only uses the data through
+#September 7, 2014.  You can download a more recent data set with
+#fileUrl<-"https://data.cityofnewyork.us/api/views/h9gi-nx95/rows.csv?accessType=DOWNLOAD"
+#download.file(fileUrl, destfile="collisions.csv", method="curl")
+collisions<-read.csv("collisions.csv", sep=",")
+
+#cleaning the file so that it is useable below
+names(collisions)<-tolower(names(collisions))
+collisions$location<-as.character(collisions$location)
+collisions$location<-gsub(",\\ ", ":", collisions$location)
+collisions$location<-gsub("\\(", "", collisions$location)
+collisions$location<-gsub("\\)", "", collisions$location)
+
+#cleaning and organizing the zipcodes
+allzipcode<-collisions$zip.code
+zipcode<-allzipcode[!is.na(allzipcode)]
+zip<-unique(zipcode)
+zip<-zip[order(zip)]
+zip<-as.list(zip)
+
+#cleaning and organizing the dates
+alldates<-as.Date(collisions$date, "%m/%d/%Y")
+dates<-unique(alldates)
+dates<-dates[order(dates)]
+startdate<-dates[1]
+enddate<-dates[length(dates)]
+
+
+shinyServer(
+  function(input, output){
+    #print selected zip codes
+    output$inputValue1<-renderPrint({input$zip})
+    
+    #get indices for rows corresponding to selected dates and zip codes
+    inzip<-reactive({allzipcode %in% input$zip})
+    daterange<-reactive({seq(input$date[1], input$date[2], by="day")})
+    indates<-reactive({alldates %in% daterange()})
+    datesandzip<-reactive({indates()&inzip()})
+    
+    #count number of zip codes listed as na in selected date range
+    nas<-reactive({sum(is.na(allzipcode[indates()]))})
+    
+    #print number of collisions in specified zip codes
+    output$totalinzip<-renderPrint({
+      sum(inzip())})
+    
+    #print number of collisions in specified date range
+    output$totalindates<-renderPrint({
+     sum(indates()) 
+    })
+    
+    #print number of collisions in selected zip codes and date range
+    output$totalinzipdates<-renderPrint({
+      sum(datesandzip()) 
+    })
+    
+    #print number of zips listed as nas in selected date range
+    output$nacount<-renderPrint(
+      {nas()}
+      )
+    
+    #creating a map showing the accidents and times they occurred 
+    output$map<-renderGvis({
+      df<-collisions[datesandzip(),]
+      gvisMap(df, "location", "time", options=list(showTip=TRUE, useMapTypeControl=TRUE, 
+                                                   enableScrollWheel=TRUE)) 
+    })                                                     
+  }  
+)
---
+++ ui.R
@@ -0,0 +1,48 @@
+#The user interface for my NYC collisions app.
+
+shinyUI(
+  #Make a page with a sidebar
+  pageWithSidebar(
+    #Application title
+    headerPanel(
+    h1("Collisions in New York City",align="center")
+    ),
+    #Use variants on html commands to design sidebar panel
+    sidebarPanel(
+      h3("Enter zip code(s) and a range of dates."),
+      selectInput('zip', 'ZIP Code: Select as many New York City zip codes as you wish.', zip, selected=10025, multiple=TRUE),
+      dateRangeInput('date', 'Date Range: Select a start date and an end date between July 1, 2012 and September 7, 2014.', 
+                     min=startdate, max=enddate, start=startdate, end=enddate),
+      #actionButton("goButton", "Submit")
+      h3('Documentation'),
+      p('This app computes the number of collisions in specified New York City (NYC) zip codes
+        during specified date ranges.  The user selects the zip codes and a range of dates
+        between July 1, 2012 and September 7, 2014.  The app also includes an interactive map showing the
+        locations of these collisions.  Note that it might take a few moments for the app to load.  The data was downloaded from'),
+       a(href="https://data.cityofnewyork.us/NYC-BigApps/NYPD-Motor-Vehicle-Collisions/h9gi-nx95",
+        "https://data.cityofnewyork.us/NYC-BigApps/NYPD-Motor-Vehicle-Collisions/h9gi-nx95"),
+      p('While that website is updated regularly with new data, this app currently uses
+        only the data through September 7, 2014.')
+    ),
+    #Use variants on html commands to create main panel
+    mainPanel(
+      h3('Collisions in selected zip code(s) and date range'),
+      h4('The ZIP code(s) you entered:'),
+      verbatimTextOutput("inputValue1"),
+      h4('The total number of collisions between July 1, 2012 and September 7, 2014 in NYC in the selected zip code(s):'),
+      verbatimTextOutput("totalinzip"),
+      h4('The total number of collisions (across all NYC zip codes) in the selected date range:'),
+      verbatimTextOutput("totalindates"),
+      h4('The total number of collisions in the selected zip codes in the selected date range:'),
+      verbatimTextOutput("totalinzipdates"),
+      h4('Here is a map of all collisions occurring in the selected zip codes in the selected date range.  
+         Mouse over each item to find out the time of day that the collision occurred.  
+         You can use the wheel on your mouse to zoom in and out.  You can also use your mouse to move around
+         the map.'),
+      htmlOutput('map'),
+      p('Note: For the following number of collisions in your selected date range, 
+          no zip code was provided in the data set.'),
+      verbatimTextOutput("nacount")
+    )
+  )  
+)
